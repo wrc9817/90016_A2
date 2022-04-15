122c3c77098d7ee784734f60f2389fc1cb118116
wrc9817 <wrc9817@gmail.com>
2022-04-15 10:59:42 +0800

    更改端口

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:8099',
     timeout: 500000,
 })
    instance.interceptors.request.use( config => {
